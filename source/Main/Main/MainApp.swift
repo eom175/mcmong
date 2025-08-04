44578b5a693ff07b5775d89aff0b95a1a2fae88e
eom175 <eom175@naver.com>
2025-08-04 15:49:47 +0900

    [init]

---
+++ source/Main/Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by 엄지용 on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
